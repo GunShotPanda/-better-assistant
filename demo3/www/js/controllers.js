f3ed9a53142e3b6a9a22880bfac72ea29427fcf3
gunshot panda <lipeiran@imudges.com>
2015-08-02 19:27:40 +0800

    first demo
    第一版demo,由ionic 自带tabs模板改动。

---
+++ demo3/www/js/controllers.js
@@ -0,0 +1,104 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+.controller('MeCtrl', function($scope) {})
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+.controller("freshCtrl",function($scope){
+      $scope.load_more = function(){
+        $timeout(function(){},500);
+      };
+})
+    .controller('LoginCtrl', function($scope, $ionicModal, $timeout) {
+
+      // With the new view caching in Ionic, Controllers are only called
+      // when they are recreated or on app start, instead of every page change.
+      // To listen for when this page is active (for example, to refresh data),
+      // listen for the $ionicView.enter event:
+      //$scope.$on('$ionicView.enter', function(e) {
+      //});
+
+      // Form data for the login modal
+      $scope.loginData = {};
+
+      // Create the login modal that we will use later
+      $ionicModal.fromTemplateUrl('templates/tab-dash.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+
+      // Triggered in the login modal to close it
+      $scope.closeLogin = function() {
+        $scope.modal.hide();
+      };
+
+      // Open the login modal
+      $scope.login = function() {
+        $scope.modal.show();
+      };
+
+      // Perform the login action when the user submits the login form
+      $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
+
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+          $scope.closeLogin();
+        }, 1000);
+      };
+    })
+    .controller('PopupCtrl',function($scope, $ionicPopup, $timeout) {
+
+      // Triggered on a button click, or some other target
+      $scope.showPopup = function () {
+        $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+          template: '<input type="password" ng-model="data.wifi">',
+          title: 'Enter Wi-Fi Password',
+          subTitle: 'Please use normal things',
+          scope: $scope,
+          buttons: [
+            {text: 'Cancel'},
+            {
+              text: '<b>Save</b>',
+              type: 'button-positive',
+              onTap: function (e) {
+                if (!$scope.data.wifi) {
+                  //don't allow the user to close unless he enters wifi password
+                  e.preventDefault();
+                } else {
+                  return $scope.data.wifi;
+                }
+              }
+            },
+          ]
+        });
+      }
+    });
+
